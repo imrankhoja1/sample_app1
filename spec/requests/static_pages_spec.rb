ec7016dcea53e4d65e326c7da0d3b995887563dd
Imran Khoja <imrankhoja1@gmail.com>
2012-02-24 18:11:47 -0500

    Remove default Rails page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,12 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "#{base_title} | Home")
     end
@@ -20,12 +20,12 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "#{base_title} | Help")
     end
@@ -34,12 +34,12 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   describe "About page" do
 
     it "should have the h1 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                     :text => "#{base_title} | About Us")
     end
@@ -48,12 +48,12 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   describe "Contact page" do
     
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text =>'Contact')
     end
     
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => "#{base_title} | Contact")
   end
 end
